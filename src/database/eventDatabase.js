5ebf3d1c8c612755f4edd23446e74d720ddad549
Samuel-Kepler <sr123456123@gmail.com>
2024-07-22 15:15:29 -0300

    feat: adicionando dados no database

---
+++ src/database/eventDatabase.js
@@ -0,0 +1,27 @@
+import { Event } from "../models/envent.model.js";
+
+export const events = [
+    new Event("Eclipse Lunar Total", "2024-04-08", "América do Norte, América Central, América do Sul"),
+    new Event("Chuva de Meteoros Liridas", "2024-04-22", "Global"),
+    new Event("Eclipse Solar Anular", "2024-10-02", "Oceano Pacífico, América do Norte, América Central"),
+    new Event("Eclipse Lunar Parcial", "2024-10-08", "Ásia, Oceania, América do Norte"),
+    new Event("Chuva de Meteoros Orionidas", "2024-10-21", "Global"),
+    new Event("Eclipse Solar Parcial", "2024-10-25", "África, Oriente Médio, Ásia Central"),
+    new Event("Chuva de Meteoros Leonidas", "2024-11-17", "Global"),
+    new Event("Eclipse Lunar Total", "2024-11-28", "África, Europa, Ásia"),
+    new Event("Chuva de Meteoros Geminidas", "2024-12-14", "Global"),
+    new Event("Chuva de Meteoros Úrsidas", "2024-12-22", "Global"),
+
+      // Eventos de 2025
+    new Event("Chuva de Meteoros Quadrantidas", "2025-01-04", "Global"),
+    new Event("Eclipse Solar Parcial", "2025-03-29", "Ásia, Europa, África"),
+    new Event("Eclipse Lunar Total", "2025-03-14", "América do Norte, América do Sul, Europa, África"),
+    new Event("Chuva de Meteoros Liridas", "2025-04-22", "Global"),
+    new Event("Eclipse Solar Anular", "2025-09-21", "Oceano Pacífico, América do Norte"),
+    new Event("Chuva de Meteoros Orionidas", "2025-10-21", "Global"),
+    new Event("Eclipse Lunar Parcial", "2025-10-17", "América do Norte, América do Sul, Europa, África"),
+    new Event("Chuva de Meteoros Leonidas", "2025-11-17", "Global"),
+    new Event("Eclipse Solar Total", "2025-11-13", "Oceano Pacífico, América do Sul"),
+    new Event("Chuva de Meteoros Geminidas", "2025-12-14", "Global"),
+    new Event("Chuva de Meteoros Úrsidas", "2025-12-22", "Global"),
+];
