15075fa9e05b552802ca4e0dc582d6eb4f414c5c
Samuel-Kepler <sr123456123@gmail.com>
2024-07-22 20:58:37 -0300

    feat: adicionando rotas ao app

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import express from "express";
+import { eventRouter } from "./src/routes/event.routes.js"; 
 
-const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(eventRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: http://localhost:${PORT}`);
+});
