f53f7861c88aea7a5797de0e047d5f16b5c7a8d9
Samuel-Kepler <sr123456123@gmail.com>
2024-07-22 23:11:22 -0300

    feat: adicionnado dados de anos anteriores ao database

--- src/database/eventDatabase.js
+++ src/database/eventDatabase.js
@@ -1,6 +1,42 @@
 import { Event } from "../models/event.model.js";
 
 export const events = [
+    // Eventos de 2020
+    new Event("Chuva de Meteoros Quadrantidas", "2020-01-04", "Global"),
+    new Event("Eclipse Solar Anular", "2020-06-21", "África, Ásia"),
+    new Event("Eclipse Lunar Penumbral", "2020-07-05", "América do Norte, América do Sul"),
+    new Event("Chuva de Meteoros Perseidas", "2020-08-12", "Global"),
+    new Event("Eclipse Lunar Penumbral", "2020-11-30", "América do Norte, América do Sul"),
+    new Event("Eclipse Solar Total", "2020-12-14", "América do Sul"),
+    new Event("Chuva de Meteoros Geminidas", "2020-12-14", "Global"),
+
+    // Eventos de 2021
+    new Event("Chuva de Meteoros Quadrantidas", "2021-01-03", "Global"),
+    new Event("Eclipse Lunar Total", "2021-05-26", "América do Norte, América do Sul, Ásia, Austrália"),
+    new Event("Eclipse Solar Anular", "2021-06-10", "África, Ásia, Europa"),
+    new Event("Chuva de Meteoros Perseidas", "2021-08-12", "Global"),
+    new Event("Eclipse Lunar Parcial", "2021-11-19", "América do Norte, América do Sul, Ásia, Austrália"),
+    new Event("Chuva de Meteoros Geminidas", "2021-12-14", "Global"),
+
+    // Eventos de 2022
+    new Event("Chuva de Meteoros Quadrantidas", "2022-01-04", "Global"),
+    new Event("Eclipse Solar Parcial", "2022-04-30", "América do Sul, Pacífico"),
+    new Event("Eclipse Lunar Total", "2022-05-16", "América do Norte, América do Sul, Europa, África"),
+    new Event("Chuva de Meteoros Perseidas", "2022-08-13", "Global"),
+    new Event("Eclipse Solar Parcial", "2022-10-25", "Europa, Ásia, África"),
+    new Event("Eclipse Lunar Total", "2022-11-08", "Ásia, Austrália, Pacífico, América do Norte, América do Sul"),
+    new Event("Chuva de Meteoros Geminidas", "2022-12-14", "Global"),
+
+    // Eventos de 2023
+    new Event("Chuva de Meteoros Quadrantidas", "2023-01-04", "Global"),
+    new Event("Eclipse Solar Híbrido", "2023-04-20", "Pacífico, Oceania, Sudeste Asiático, Antártida"),
+    new Event("Eclipse Lunar Penumbral", "2023-05-05", "Europa, África, Ásia, Austrália"),
+    new Event("Chuva de Meteoros Perseidas", "2023-08-13", "Global"),
+    new Event("Eclipse Solar Anular", "2023-10-14", "Pacífico, América do Norte, América do Sul"),
+    new Event("Eclipse Lunar Parcial", "2023-10-28", "Europa, Ásia, África, Oceania"),
+    new Event("Chuva de Meteoros Geminidas", "2023-12-14", "Global"),
+
+    // Eventos de 2024
     new Event("Eclipse Lunar Total", "2024-04-08", "América do Norte, América Central, América do Sul"),
     new Event("Chuva de Meteoros Liridas", "2024-04-22", "Global"),
     new Event("Eclipse Solar Anular", "2024-10-02", "Oceano Pacífico, América do Norte, América Central"),
